8ddc06eb000bf8ea71c761081188209701b99ec3
Nightac <nightaccoskun@gmail.com>
2025-03-25 01:16:18 +0300

    up drag pad

--- Runtime/Scripts/DragPad.cs
+++ Runtime/Scripts/DragPad.cs
@@ -30,6 +30,10 @@ public class DragPad : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, ID
     }
     public void OnPointerDown(PointerEventData eventData)
     {
+        Delta = Vector2.zero;
+        IsDragging = false;
+        IsDragUp = false;
+
         IsDragDown = true;
         _onDragDownEvent?.Invoke(this);
     }
@@ -43,6 +47,10 @@ public class DragPad : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, ID
     public void OnPointerUp(PointerEventData eventData)
     {
         TotalDelta = Vector2.zero;
+        Delta = Vector2.zero;
+        IsDragging = false;
+        IsDragDown = false;
+
         IsDragUp = true;
         _onDragUpEvent?.Invoke(this);
     }

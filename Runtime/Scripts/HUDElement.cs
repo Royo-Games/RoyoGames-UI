83b01c79e8c157443788e63aa2ebad2b26597ad6
Nightac <nightaccoskun@gmail.com>
2025-03-28 09:26:10 +0300

    up

--- Runtime/Scripts/HUDElement.cs
+++ Runtime/Scripts/HUDElement.cs
@@ -40,7 +40,7 @@ public class HUDElement : MonoBehaviour
         canvasRect = canvas.GetComponent<RectTransform>();
     }
 
-    public virtual void LateUpdate()
+    public virtual void Update()
     {
         Vector2 screenPoint = RectTransformUtility.WorldToScreenPoint(_worldCamera, _target.position + _targetOffset);
 

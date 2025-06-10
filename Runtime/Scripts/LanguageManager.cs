1d6a0b6c5af192f5b3a33e87dcd4137674b9be78
Nightac <nightaccoskun@gmail.com>
2025-06-10 16:40:38 +0300

    up Language Manager

--- Runtime/Scripts/LanguageManager.cs
+++ Runtime/Scripts/LanguageManager.cs
@@ -1,30 +1,83 @@
 using RoyoGames.Document;
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 
 [DefaultExecutionOrder(-1000)]
-public class LanguageManager : Singleton<LanguageManager>
+public class LanguageManager : MonoBehaviour
 {
     public static bool IsDestroy;
-
-    public static SystemLanguage CurrentLanguage { get; private set; }
+    private static LanguageManager _instance;
 
     [SerializeField] private TextAsset textAsset;
     [SerializeField] private SystemLanguage defaultLanguage;
     [SerializeField] private bool autoLoad = true;
 
-    [HideInInspector] public UnityAction OnChangedLanguageEvent;
+    [HideInInspector] public static UnityAction OnChangedLanguageEvent;
 
-    private EntityTable table;
+    private static EntityTable table;
 
     private static string currentLanguageName;
     private static string defaultLanguageName;
 
-    public override void Awake()
+    public static SystemLanguage DefaultLanguage
     {
-        base.Awake();
+        get
+        {
+            return _instance.defaultLanguage;
+        }
+
+        set
+        {
+            _instance.defaultLanguage = value;
+            defaultLanguageName = value.ToString();
+        }
+    }
+
+    private static SystemLanguage currentLanguage;
+    public static SystemLanguage CurrentLanguage
+    {
+        get
+        {
+            return currentLanguage;
+        }
+
+        set
+        {
+            currentLanguage = value;
+            currentLanguageName = value.ToString();
+
+            if (table != null)
+                return;
+
+            foreach (var textTranslate in FindObjectsByType<TextTranslator>(FindObjectsInactive.Include, FindObjectsSortMode.None))
+            {
+                textTranslate.Translate();
+            }
+
+            OnChangedLanguageEvent?.Invoke();
+        }
+    }
+
+    public static TextAsset TextAsset
+    {
+        get
+        {
+            return _instance.textAsset;
+        }
+        set
+        {
+            _instance.textAsset = value;
+        }
+    }
+
+    public void Awake()
+    {
+        _instance = this;
         DontDestroyOnLoad(gameObject);
-        Load();
+
+        if (autoLoad)
+            Load();
     }
 
     private void OnDestroy()
@@ -32,40 +85,43 @@ public class LanguageManager : Singleton<LanguageManager>
         IsDestroy = true;
     }
 
-    private void Load()
+    public static void Load()
+    {
+        Load(Application.systemLanguage);
+    }
+
+    public static void Load(SystemLanguage language)
     {
         table = new EntityTable();
-        table.LoadFromString(textAsset.text);
+        table.LoadFromString(_instance.textAsset.text);
 
-        SetDefaultLanguage(defaultLanguage);
-        SetLanguage(Application.systemLanguage);
-    }
-
-    public void SetDefaultLanguage(SystemLanguage language)
-    {
-        defaultLanguage = language;
-        defaultLanguageName = defaultLanguage.ToString();
-    }
-
-    public void SetLanguage(SystemLanguage language)
-    {
+        DefaultLanguage = _instance.defaultLanguage;
         CurrentLanguage = language;
-        currentLanguageName = language.ToString();
+    }
 
-        foreach (var textTranslate in FindObjectsByType<TextTranslator>(FindObjectsInactive.Include, FindObjectsSortMode.None))
-        {
-            textTranslate.Translate();
-        }
+    public static SystemLanguage[] GetLanguages()
+    {
+        if (table == null)
+            return new SystemLanguage[0];
 
-        OnChangedLanguageEvent?.Invoke();
+        string[] columnNames = table.GetColumns();
+        SystemLanguage[] languages = new SystemLanguage[columnNames.Length - 1];
+
+        for (int i = 1; i < columnNames.Length; i++)
+            languages[i - 1] = Enum.Parse<SystemLanguage>(columnNames[i]);
+
+        return languages;
     }
 
     public static string Translate(string key)
     {
-        EntityTable.Row row = Instance.table.GetRow("Key", key);
+        if (table == null)
+            return "";
+
+        EntityTable.Row row = table.GetRow("Key", key);
         string value = row.GetValue(currentLanguageName);
 
-        if(string.IsNullOrEmpty(value))
+        if (string.IsNullOrEmpty(value))
             value = row.GetValue(defaultLanguageName);
 
         return value;
